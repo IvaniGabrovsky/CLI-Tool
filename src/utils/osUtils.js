ba184a9b6bb097d54caa735e85920134f88eec4d
Denes Dolhay <denke@denkesys.com>
2022-10-04 14:20:09 -0400

    feat: Config file support

--- src/utils/osUtils.js
+++ src/utils/osUtils.js
@@ -2,53 +2,91 @@ const fs = require("fs");
 
 function isDir(pathItem) {
   try {
-      var stat = fs.lstatSync(pathItem);
-      return stat.isDirectory();
+    var stat = fs.lstatSync(pathItem);
+    return stat.isDirectory();
   } catch (e) {
-      // lstatSync throws an error if path doesn't exist
-      return false;
+    // lstatSync throws an error if path doesn't exist
+    return false;
   }
 }
 
 function isFile(pathItem) {
   try {
-      var stat = fs.lstatSync(pathItem);
-      return !stat.isDirectory();
+    var stat = fs.lstatSync(pathItem);
+    return !stat.isDirectory();
   } catch (e) {
-      // lstatSync throws an error if path doesn't exist
-      return false;
+    // lstatSync throws an error if path doesn't exist
+    return false;
   }
 }
 
 function isExists(pathItem) {
   try {
-      if (fs.existsSync(pathItem)) {
-          return true;
-      } else {
-          return false;
-      }
-  } catch (err) {
-      console.error(err);
+    if (fs.existsSync(pathItem)) {
+      return true;
+    } else {
       return false;
+    }
+  } catch (err) {
+    console.error(err);
+    return false;
   }
 }
 
 function makeDir(pathItem) {
   try {
-      if (!fs.existsSync(pathItem)) {
-          fs.mkdirSync(pathItem);
-      }
+    if (!fs.existsSync(pathItem)) {
+      fs.mkdirSync(pathItem);
+    }
   } catch (err) {
-      console.error(err);
+    console.error(err);
   }
 }
 
 function removeDir(pathItem) {
   fs.rmSync(pathItem, { recursive: true }, (err) => {
-      if (err) {
-          throw err;
-      }
+    if (err) {
+      throw err;
+    }
   });
 }
 
-module.exports = { isDir, isFile, isExists, makeDir, removeDir};
+function envParserAction(runSsg) {
+  // Curried function, would look like (callback) => (options, command) => {} on es6
+  return function (options, command) {
+    // No config file specified, let's continue (early return)
+    if (!options.config) return runSsg(command);
+
+    // Try to read, parse and set config values
+    try {
+      // check if file exists
+      if (!isFile(options.config)) throw new Error("Config file not found");
+
+      // Try to read and parse file
+      const content = fs.readFileSync(options.config);
+      const parsedConfig = JSON.parse(content);
+
+      // Loop through all the keys, and set each option in Commander
+      Object.keys(parsedConfig).forEach(function (key) {
+        command.setOptionValue(key, parsedConfig[key]);
+      });
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
+
+    console.log(command.opts());
+
+    // The new options has been set, let's contiue
+    return runSsg(command);
+  };
+}
+
+module.exports = {
+  isDir,
+  isFile,
+  isExists,
+  makeDir,
+  removeDir,
+  envParserAction,
+};
