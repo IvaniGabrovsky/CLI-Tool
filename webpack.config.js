0e115b25c6059895fa2b4c21648bf4983c7c50f9
IvaniGabrovsky <ivan.gabrovsky@gmail.com>
2022-11-18 22:09:44 -0500

    removed anything related to html in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,25 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
 
-const isProduction = process.env.NODE_ENV == 'production';
-
+const isProduction = process.env.NODE_ENV == "production";
 
 const config = {
-    entry: './src/index.js',
+    entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, "dist"),
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-    ],
+    plugins: [],
+    target: "node",
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
+                loader: "babel-loader",
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
+                type: "asset",
             },
 
             // Add your rules for custom modules here
@@ -38,11 +30,9 @@ const config = {
 
 module.exports = () => {
     if (isProduction) {
-        config.mode = 'production';
-        
-        
+        config.mode = "production";
     } else {
-        config.mode = 'development';
+        config.mode = "development";
     }
     return config;
 };
